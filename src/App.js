e7e329911d42105788fe0e10e5ae11bfc471fddc
jela3105 <jela3105@gmail.com>
2021-03-05 16:06:28 -0600

    Create template of timeline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="linea-tiempo">
+        <div className="momento">
+          <h3>2010</h3>
+          <div className="descripcion">
+            Poop on grasses fight an alligator and win and fight own tail.
+          </div>
+        </div>
+        <div className="momento">
+          <h3>2011</h3>
+          <div className="descripcion">
+            Weigh eight pounds but take up a full-size bed white cat sleeps on a
+            black shirt. Ask for petting sit and stare stand in doorway,
+            unwilling to chose whether to stay in or go out for really likes
+            hummus and human clearly uses close to one life a night no one naps
+            that long.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
